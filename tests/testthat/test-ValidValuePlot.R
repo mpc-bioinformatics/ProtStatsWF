1bef8ae469750a1891985ed06cf64d4c42611ef4
Katharina Lara Neuhaus <katharina.neuhaus@ruhr-uni-bochum.de>
2023-11-06 12:01:01 +0100

    tests now have relative data path

--- tests/testthat/test-ValidValuePlot.R
+++ tests/testthat/test-ValidValuePlot.R
@@ -1,11 +1,11 @@
 test_that("the spreadsheet is correct", {
-  pData <- prepareData(data_path = "/Users/kalar/Documents/0_Studium/SHK-WHK/Testdata/preprocessed_peptide_data_D1.xlsx", intensity_columns = 3:17, use_groups = TRUE)
-  pGraph <- ValidValuePlot(D_long = D1[[6]])
+  pData <- prepareData(data_path = test_path("testdata", "test01.xlsx"), intensity_columns = 3:11, use_groups = TRUE)
+  pGraph <- ValidValuePlot(D_long = pData[[6]])
   expect_snapshot(pGraph[[1]])
 })
 
 test_that("the plot is correct", {
-  pData <- prepareData(data_path = "/Users/kalar/Documents/0_Studium/SHK-WHK/Testdata/preprocessed_peptide_data_D1.xlsx", intensity_columns = 3:17, use_groups = TRUE)
-  pGraph <- ValidValuePlot(D_long = D1[[6]])
+  pData <- prepareData(data_path = test_path("testdata", "test01.xlsx"), intensity_columns = 3:11, use_groups = TRUE)
+  pGraph <- ValidValuePlot(D_long = pData[[6]])
   vdiffr::expect_doppelganger("ValidValuePlot", pGraph[2])
 })
