a790b7ac904f8de9fcacaf447633fa8538c3914b
Karin Schork <schorkka@mpc-domain.local>
2024-07-11 11:56:24 +0200

    on/off heatmap

--- R/WIP_OnOff_Heatmap.R
+++ R/WIP_OnOff_Heatmap.R
@@ -23,15 +23,6 @@ calculate_onoff <- function(D, id, group, max_vv_off, min_vv_on, protein_id_col 
   #Gene.names <- id[, gene_names_col]
   Protein.IDs <- id[, protein_id_col]
 
-  ### if gene name is empty, replace it with protein accession
-  # ind_NA_gene <- which(is.na(Gene.names) | Gene.names == "")
-  # if (length(ind_NA_gene) > 0) {
-  #   #Gene.names[ind_NA_gene] <-
-  #   id[ind_NA_gene, gene_names_col] <- Protein.IDs[ind_NA_gene]
-  # }
-  # id[, gene_names_col] <- make.unique(id[, gene_names_col])
-  # # Gene.names <- make.unique(Gene.names)
-
   ## converting to long format
   D_long <- tidyr::pivot_longer(data = cbind(Protein.IDs = Protein.IDs, D), cols = colnames(D))
   D_long$group <- group[match(D_long$name, colnames(D))]
@@ -42,22 +33,17 @@ calculate_onoff <- function(D, id, group, max_vv_off, min_vv_on, protein_id_col 
 
   ## convert to wide format again
   D_onoff_wide <- tidyr::pivot_wider(D_onoff,
-                              id_cols = Protein.IDs,
-                              values_from = c(valid_values, valid_values_rel),
-                              names_from = group)
+                                     id_cols = Protein.IDs,
+                                     values_from = c(valid_values, valid_values_rel),
+                                     names_from = group)
   ind <- match(D_onoff_wide$Protein.IDs, Protein.IDs)
 
-  # if (relative) {
-  #   cols <- colnames(D_onoff_wide)[(nr_groups+2):(2*nr_groups+1)]
-  # } else {
-     cols <- colnames(D_onoff_wide)[2:(nr_groups+1)]
-  # }
+  cols <- colnames(D_onoff_wide)[2:(nr_groups+1)]
+
 
   ### calculate, if protein is on/off
   res_onoff <- apply(D_onoff_wide[,cols], 1, function(x) {
-    #if (is.null(onoffdiff))
-      isonoff <- any(x <= max_vv_off) & any(x >= min_vv_on)
-    #if (!is.null(onoffdiff)) isonoff <- diff(range(x)) >= onoffdiff
+    isonoff <- any(x <= max_vv_off) & any(x >= min_vv_on)
     return(isonoff)
   })
 
@@ -65,8 +51,6 @@ calculate_onoff <- function(D, id, group, max_vv_off, min_vv_on, protein_id_col 
   D_onoff_wide_tmp <- cbind(D_onoff_wide, isonoff = res_onoff)
   D_onoff_wide_tmp2 <- cbind(id[ind,], D_onoff_wide_tmp[,-1])
   return(D_onoff_wide_tmp2)
-  #write.xlsx(D_onoff_wide_tmp2, paste0(output_path, "table_onoff", suffix, ".xlsx"), keepNA = TRUE, overwrite = TRUE)
-
 }
 
 
@@ -78,67 +62,61 @@ calculate_onoff <- function(D, id, group, max_vv_off, min_vv_on, protein_id_col 
 ################################################################################
 ################################################################################
 #
-#
-# Onoff_plus_heatmap <- function(D, id, group,
-#                                gene_names_col, protein_id_col,
-#                                max_vv_off, min_vv_on, onoffdiff = NULL, relative = FALSE,
-#                                output_path,
-#                                plot_height = 10, plot_width = 10, suffix = "", ... ){
-#   require(tidyverse)
-#
-#
-#
-#   D_onoff_wide2 <- D_onoff_wide_tmp2[D_onoff_wide_tmp2$isonoff, ]
-#
-#   #### TODO: D_onoff_wide2 ist leer, weil isonoff für alles Falsch ist
-#   ##validValue_col <- which(colnames(D_onoff_wide2) %in% cname)
-#  # cname <<- colnames(D_onoff_wide2)[-c(1, ncol(D_onoff_wide2))]
-#   ####
-#   D_onoff2_long <- as.data.frame(pivot_longer(D_onoff_wide2, cols = cols)) # [1]:cname[length(cname)]
-#   if (relative) {
-#     D_onoff2_long$group <- str_replace(D_onoff2_long$name, "valid_values_rel_", "")
-#   } else {
-#     D_onoff2_long$group <- str_replace(D_onoff2_long$name, "valid_values_", "")
-#   }
-#
-#   D_onoff2_long$group <- factor(D_onoff2_long$group, levels = levels(group))
-#
-#
-#   ### TODO: clustering für Reihenfolge/order der Proteine?
-#   ord <- do.call(order, args = c(as.list(D_onoff_wide2[, cols]), decreasing = TRUE))
-#
-#   #cl <- hclust(dist(D_onoff_wide2[, cols], method = "manhattan"), method="complete")
-#   D_onoff2_long[, gene_names_col] <- factor(D_onoff2_long[, gene_names_col],
-#                                      levels = D_onoff_wide2[, gene_names_col][ord])
-#
-#
-#   D_onoff2_long_2 <<- D_onoff2_long
-#
-#   pl <- ggplot(data = D_onoff2_long, aes(x = group, y = Gene.names, fill = value)) +
-#     geom_tile() +  ylab("Gene name") + xlab("group") + theme_bw()
-#
-#   #if (onoffGreaterThanEqual < 1 | !is.null(onoffdiff)) {
-#   pl <- pl + scale_fill_gradient(limits = c(0,max(D_onoff2_long$value)), low = "white", high = "forestgreen") #
-#   pl <- pl + theme(axis.text = element_text(size = rel(1.8)),
-#                    axis.title = element_text(size = rel(1.8)),
-#                    legend.title = element_text(size=rel(1.8)),
-#                    legend.text = element_text(size=rel(1.8)))
-#   pl
-#
-#
-#   #print(length(unique(D_onoff2_long)))
-#   #} else {
-#     # colours_green <- scales::seq_gradient_pal("white", "forestgreen", "Lab")(seq(0,1,length.out=6))
-#   #  colours_green <- scales::seq_gradient_pal("white", "forestgreen", "Lab")(seq(0,1,length.out=(length(unique(D_onoff2_long))+1) ))
-#    # pl <- pl + scale_fill_manual(values=colours_green, name = "Valid \nvalues")
-#   #}
-#
-#   ggsave(filename = paste0(output_path, "Heatmap_onoff", suffix,".png"),
-#          device = "png", plot = pl,
-#          height = plot_height, width = plot_width, dpi = 300)
-# }
-#
-#
-#
-#
-#
+
+Onoff_plus_heatmap <- function(RES_onoff,
+                               protein_name_column = "Gene.names",
+                               relative = FALSE){
+
+  require(tidyverse)
+
+  ## choose only the rows with on/off proteins
+  RES_onoff2 <- RES_onoff[RES_onoff$isonoff, ]
+
+  #### TODO: D_onoff_wide2 ist leer, weil isonoff für alles Falsch ist
+
+  validvalue_cols <- setdiff(colnames(RES_onoff2)[grep("valid_values_", colnames(RES_onoff2))], colnames(RES_onoff2)[grep("valid_values_rel_", colnames(RES_onoff2))])
+
+  ### TODO: schlauere Methode um doppelte Proteinname zu behandeln? (Das sind meist die leeren! -> überschreiben mit protein accession z.B.)
+  RES_onoff2[, protein_name_column] <- make.names(RES_onoff2[, protein_name_column], unique = TRUE)
+
+
+  RES_onoff2_long <- as.data.frame(pivot_longer(RES_onoff2, cols = all_of(validvalue_cols), names_to = "group"))
+
+
+  if (relative) {
+    RES_onoff2_long$group <- str_replace(RES_onoff2_long$group, "valid_values_rel_", "")
+  } else {
+    RES_onoff2_long$group <- str_replace(RES_onoff2_long$group, "valid_values_", "")
+  }
+
+  ### TODO: level Reihenfolge der Gruppe nutzen statt alphabetisch
+  #RES_onoff2_long$group <- factor(RES_onoff2_long$group, levels = levels(group))
+
+
+  ### TODO: clustering für Reihenfolge/order der Proteine?
+  ord <- do.call(order, args = c(as.list(RES_onoff2[, validvalue_cols]), decreasing = TRUE))
+  #cl <- hclust(dist(D_onoff_wide2[, cols], method = "manhattan"), method="complete")
+
+
+  RES_onoff2_long[, protein_name_column] <- factor(RES_onoff2_long[, protein_name_column],
+                                     levels = RES_onoff2[, protein_name_column][ord])
+
+  pl <- ggplot(data = RES_onoff2_long, aes(x = group, y = Gene.names, fill = value)) +  ## TODO: Gene.names
+    geom_tile() +  ylab("Gene name") + xlab("group") + theme_bw()
+
+  #if (onoffGreaterThanEqual < 1 | !is.null(onoffdiff)) {
+  pl <- pl + scale_fill_gradient(limits = c(0,max(RES_onoff2_long$value)), low = "white", high = "forestgreen") #
+  pl <- pl + theme(axis.text = element_text(size = rel(1.8)),
+                   axis.title = element_text(size = rel(1.8)),
+                   legend.title = element_text(size=rel(1.8)),
+                   legend.text = element_text(size=rel(1.8)))
+  pl
+
+  return(pl)
+
+}
+
+
+
+
+
