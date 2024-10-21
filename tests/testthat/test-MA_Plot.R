435a9e0420f4eb699372013ec465918068a14ad5
ItsMeKathy <94694949+ItsMeKathy@users.noreply.github.com>
2024-10-21 12:33:51 +0200

    cleaning up MA-plot-testing

--- tests/testthat/test-MA_Plot.R
+++ tests/testthat/test-MA_Plot.R
@@ -5,11 +5,11 @@ test_that("Single MA plot", {
   s1 = D[,1]
   s2 = D[,2]
 
-  grDevices::png(file = test_path("testdata", "MA_plot_snapshot.png"))
+  grDevices::png(file = test_path("testdata", "result_MA_plot_snapshot.png"))
   MA_Plot_single(sample_1 = s1, sample_2 = s2, do_log_transformation = FALSE, alpha = FALSE)
   grDevices::dev.off()
 
-  expect_snapshot_file(path = test_path("testdata", "MA_plot_snapshot.png"), name = "MA_Plot")
+  expect_snapshot_file(path = test_path("testdata", "result_MA_plot_snapshot.png"), name = "MA_Plot")
 })
 
 test_that("plot", {
@@ -17,7 +17,7 @@ test_that("plot", {
   pData <- prepareData(data_path = test_path("testdata", "test_file_MA_plots.xlsx"), intensity_columns = 3:6)
   D = pData[["D"]]
 
-  pResult <- MA_Plots(D = D, do_log_transformation = FALSE, output_path = test_path("testdata"))
+  pResult <- MA_Plots(D = D, do_log_transformation = FALSE, output_path = test_path("testdata"), suffix = "_result")
 
   expect_snapshot(pResult)
 })
