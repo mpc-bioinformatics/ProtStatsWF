9e4ba8fd11fd4606d20a78bc68d49eec93eaa186
ItsMeKathy <94694949+ItsMeKathy@users.noreply.github.com>
2024-11-11 16:30:07 +0100

    cleaned up candidate boxplots test

--- tests/testthat/test-candidate-boxplots.R
+++ tests/testthat/test-candidate-boxplots.R
@@ -28,28 +28,6 @@ test_that("Calculate candidate boxplots for an ANOVA ", {
   
   pData <- openxlsx::read.xlsx(xlsxFile = test_path("testdata", "result_ANOVA.xlsx"), na.strings = c("NA", "NaN", "Filtered","#NV"))
   
-  p_posthoc_columns <- grep("p.posthoc.", colnames(pData))
-  fc_columns <- grep("FC_", colnames(pData))
-  
-  # filter FC columns because FC is calculated "twice" e.g. for state1_divided_state2 and state1_divided_state2
-  if(fc_columns[[1]]%%2 == 0){
-    fc_columns <- fc_columns[fc_columns %% 2 == 0]
-  }else{
-    fc_columns <- fc_columns[fc_columns %% 2 != 0]
-  }
-  
-  candidates <- list()
-  
-  for (i in 1:length(p_posthoc_columns)) {
-    significance <- factor(calculate_significance_categories_ANOVA(p_anova = pData[["p.anova"]],
-                                                                   p_anova_adj = pData[["p.anova.fdr"]],
-                                                                   p_posthoc = pData[[p_posthoc_columns[[i]]]],
-                                                                   fc = pData[[fc_columns[[i]]]]))
-    candidates[[i]] <- as.character(significance)
-    candidates[[i]] <- which(candidates[[i]] == "significant after FDR correction")
-  }
-  
-  candidates <- unique(unlist(candidates))
   candidates <- 11:12
 
   data <- list("D" = pData[,3:11], "ID" = pData[,1:2], 
