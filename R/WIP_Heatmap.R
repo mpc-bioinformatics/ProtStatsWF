a53d392a0f8973e35fcfdf4a892f14d99da5147e
ItsMeKathy <94694949+ItsMeKathy@users.noreply.github.com>
2024-12-13 14:21:51 +0100

    documenting heatmap

--- R/WIP_Heatmap.R
+++ R/WIP_Heatmap.R
@@ -40,44 +40,74 @@
 
 
 
-#' Heatmap
+#' Create a Heatmap.
 #'
-#' @param D data set containing only protein intensities, already filtered for interesting candidates
-#' @param id data set containing further columns, e.g. protein or gene names
-#' @param protein_names_col column with protein or gene names (NULL if no protein names should be plotted)
-#' @param na_method "na.omit" -> proteins with any missing values will be removed
-#                   "impute" -> imputation of missing values
-#                   "keep" -> keep missing values
-#'                  note that clustering may not work when too many missing values are present
-#' @param filtermissings filter out proteins with more than X missing values (rows with only 1 or 2 valid values may cause problems with clustering)
-#' @param groups factor of group or data.frame with two or more grouping variables
-#' @param group_colours named list of group colours (discrete vars) or colour functions (continuous vars)
-#' @param column_split should columns be split? NULL if not, name of column in groups if yes
-#' @param cluster_column_slices cluster the column slices?
-#' @param cluster_rows if TRUE, rows will be clustered
-#' @param cluster_columns if TRUE, columns will be clustered
-#' @param dist_method distance metric for clustering, e.g. "pearson", "spearman", "euclidean"
-#' @param clust_method linkage method for clustering, e.g. "complete", "single", "average"
-#' @param symmetric_legend should colour code be made symmetric? (only make sense for z-scored data)
-#' @param scale_data should data be scaled ( = z-scored)?
-#' @param output_path path for exporting the plot
-#' @param suffix suffix for file name
-#' @param legend_name name for legend
-#' @param title title
-#' @param legend_colours colours for colour gradient
-#' @param plot_height the plot height
-#' @param plot_width the plot width
-#' @param plot_dpi the "dots per inch" for the image resolution
-#' @param log_data If \code{TRUE}, the data will be log-transformed.
-#' @param log_base The base in which the log-transformation is done.
-#' @param colour_scale_max the cap value for which all greater values will receive the same color
-#' @param textsize the size of text in the plot
-#' @param ... further arguments to Heatmap
+#' @param D                       \strong{data.frame} \cr
+#'                                The data set containing only protein intensities, already filtered for interesting candidates.
+#' @param id                      \strong{data.frame} \cr
+#'                                The corresponding ID columns for the parameter D e.g. containing further columns like protein or gene names
+#' @param protein_names_col       \strong{integer} \cr
+#'                                The column with protein or gene names, if the names should be plotted.
+#' @param na_method               \strong{character} \cr
+#'                                The method with which missing values are handeled. 
+#'                                Options are "na.omit" (proteins with any missing values will be removed), "impute" (missing values will be imputed) and "keep" (missing values will be kept).
+#'                                Note that clustering may not work when too many missing values are present.
+#' @param filtermissings          \strong{integer} \cr
+#'                                The threshold for missing values. 
+#'                                If a protein has more missing values, it will be filtered out.
+#'                                Note that rows with only 1 or 2 valid values may cause problems with clustering.
+#' @param groups                  \strong{integer} \cr
+#'                                ? A factor of group or data.frame with two or more grouping variables.
+#' @param group_colours           named list of group colours (discrete vars) or colour functions (continuous vars)
+#' @param column_split            \strong{character} \cr
+#'                                ? The name of the column in groups, if the columns should be split.
+#' @param cluster_column_slices   \strong{logical} \cr
+#'                                ? If \code{TRUE}, column slices will be clustered.
+#' @param cluster_rows            \strong{logical} \cr
+#'                                If \code{TRUE}, the rows will be clustered.
+#' @param cluster_columns         \strong{logical} \cr
+#'                                If \code{TRUE}, the columns will be clustered.
+#' @param dist_method             \strong{character} \cr
+#'                                The distance metric for clustering. Options are "pearson", "spearman" and "euclidean".
+#' @param clust_method            \strong{character} \cr
+#'                                The linkage method for clustering. Options are "complete", "single" and "average".
+#' @param symmetric_legend        \strong{logical} \cr
+#'                                If \code{TRUE}, the colour code will be  symmetric.
+#'                                Note that it only make sense for z-scored data.
+#' @param scale_data              \strong{logical} \cr
+#'                                If \code{TRUE}, the data will be scaled ( = z-scored).
+#' @param output_path             \strong{character} \cr
+#'                                The path to the output folder.
+#' @param suffix                  \strong{character} \cr
+#'                                The suffix of the file names should have one.
+#' @param legend_name             \strong{character} \cr
+#'                                The name for legend.
+#' @param title                   \strong{character} \cr
+#'                                The title of the plot.
+#' @param legend_colours          \strong{character} \cr
+#'                                A vector of colours for colour gradient.
+#' @param plot_height             \strong{numeric} \cr
+#'                                The plot height in cm.
+#' @param plot_width              \strong{numeric} \cr
+#'                                The plot width in cm.
+#' @param plot_dpi                \strong{integer} \cr
+#'                                The "dots per inch" of the plot aka. the plot resolution.
+#' @param log_data                \strong{logical} \cr
+#'                                If \code{TRUE}, the data will be log-transformed.
+#' @param log_base                \strong{integer} \cr
+#'                                The base for the log-transformation, if \code{log_data = TRUE}.
+#' @param colour_scale_max        \strong{numeric} \cr
+#'                                The cap value for which all greater values will receive the same color.
+#' @param textsize                \strong{integer} \cr
+#'                                The size of text in the plot.
+#' @param ...                     Further arguments to Heatmap
 #'
-#' @return heatmap
+#' @return A heatmap of the given data.
 #' @export
 #'
-#' @examples # TODO
+#' @examples 
+#' 
+
 Heatmap_with_groups <- function(D, id, protein_names_col = NULL,
                            na_method = "na.omit", filtermissings = 2,
                            groups = NULL, group_colours = NULL,
