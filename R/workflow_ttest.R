0a56555c683e4438749dc08fe71265e75a578a40
Karin Schork <schorkka@mpc-domain.local>
2025-04-10 15:22:54 +0200

    export workflow_ANOVA

--- R/workflow_ttest.R
+++ R/workflow_ttest.R
@@ -311,7 +311,7 @@ workflow_ttest <- function(data_path,
 #'
 #'
 #' @return Message log of the workflow
-#'
+#' @export
 #'
 #' @seealso [workflow_ttest()] in case of only two groups in the sample.\cr
 #'          Functions used in this workflow:
