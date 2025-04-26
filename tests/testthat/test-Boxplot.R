8a414f7ad77341b9316509870e1d2b0dcef9230a
ItsMeKathy <94694949+ItsMeKathy@users.noreply.github.com>
2025-04-26 19:19:20 +0200

    typo

--- tests/testthat/test-Boxplot.R
+++ tests/testthat/test-Boxplot.R
@@ -8,7 +8,7 @@ test_that("Boxplot with groups for test_file_1", {
 
 test_that("Violinplot without groups for test_file_1", {
   pData <- prepareData(data_path = system.file("extdata", "test_file_1.xlsx", package = "ProtStatsWF"), intensity_columns = 3:11, use_groups = FALSE)
- pResult <- Boxplots(D_long = pData[["D_long"]], method = "violinplot")
+  pResult <- Boxplots(D_long = pData[["D_long"]], method = "violinplot")
   plot <- pResult[["plot"]]
   vdiffr::expect_doppelganger("Violinplot_test_file_1", plot)
 })
