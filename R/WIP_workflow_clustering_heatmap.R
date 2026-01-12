4d72d654508b6d70c19a1962a23dc65b6de8d158
KarinSchork <karin.schork@rub.de>
2026-01-12 16:25:30 +0100

    start with plan for clustering workflow

---
+++ R/WIP_workflow_clustering_heatmap.R
@@ -0,0 +1,32 @@
+
+
+
+### Prerequisite:
+## have data set ready normalized, log2 transformed, if needed missing value imputation
+
+
+### parameters
+# colours for the clusters
+
+
+
+### Steps:
+
+# filter out samples with only missing values
+# then, if necessary build the topannotation for the heatmap
+
+# cluster the data with hclust (and columns)
+# build dendrograms for clustering
+# if needed, colour the dendrogram
+
+# then plot the heatmap
+# plot lineplots (one for each cluster)
+
+
+
+
+
+
+
+
+
