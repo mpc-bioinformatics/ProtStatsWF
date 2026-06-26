727d1f76e3ed6ab65762fbec8d633a35ef1e910b
KarinSchork <karin.schork@rub.de>
2026-06-26 16:04:55 +0200

    update boxplot tests

--- tests/testthat/test-plot-Boxplots.R
+++ tests/testthat/test-plot-Boxplots.R
@@ -14,6 +14,26 @@ test_that("Test Boxplots", {
                        lwd = 0.5,
                        outlierSize = 1)
   vdiffr::expect_doppelganger("Boxplots_test_file_1", boxplots)
+
+
+  boxplots2 <- Boxplots(D$D_long,
+                       method = "boxplot",
+                       groupColumn = "Group",
+                       groupColours = c("green", "yellow"),
+                       baseSize = 25,
+                       lwd = 2,
+                       outlierSize = 0.2)
+  vdiffr::expect_doppelganger("Boxplots_test_file_2", boxplots2)
+
+  violin <- Boxplots(D$D_long,
+                       method = "violinplot",
+                       groupColumn = "Group",
+                       groupColours = NULL,
+                       baseSize = 15,
+                       lwd = 0.5,
+                       outlierSize = 1)
+  vdiffr::expect_doppelganger("Boxplots_test_file_3", violin)
+
 })
 
 ## TODO: test different colours and base_size and other settings
